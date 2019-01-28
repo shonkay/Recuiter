e1ad7775e22daf28b5269eddbee72d6e61179da1
Kayode Shontan <kshontan@sbsc.com>
2019-01-28 11:48:41 +0100

    Profile edit changed

--- Recuiter/Controllers/ApplicantsController.cs
+++ Recuiter/Controllers/ApplicantsController.cs
@@ -189,7 +189,16 @@ namespace Recruiter.Controllers
 						applicant.EducationLevel = applicantProfileViewModel.EducationLevel;
 						applicant.YearsOfExperience = applicantProfileViewModel.YearsOfExperience;
 
-						dbContext.Applicants.Add(applicant);
+
+						dbContext.SaveChanges();
+					}
+					else if (applicant == null)
+					{
+						var appnew = new Applicant()
+						{
+							PhoneNumber = applicant.PhoneNumber,
+						};
+						dbContext.Applicants.Add(appnew);
 						dbContext.SaveChanges();
 					}
 					else
@@ -201,7 +210,7 @@ namespace Recruiter.Controllers
 				}
 
 			}
-			return View(applicantProfileViewModel);
+			return RedirectToAction("ApplicantProfilePage");
 		}
 
 		[HttpPost]
@@ -215,7 +224,7 @@ namespace Recruiter.Controllers
 					var applicantEntity = dbContext.Applicants.Where(a => a.UserId == currentUserId).Include(x => x.User).Include(x => x.PastEducation).Include(x => x.Skills).Include(x => x.ApplicantDocuments)
 								 .Include(x => x.Institutions).FirstOrDefault();
 
-					if(applicantEntity == null)
+					if (applicantEntity == null)
 					//var   returnapp = new ApplicantResumeVM
 
 					{
@@ -260,7 +269,7 @@ namespace Recruiter.Controllers
 
 
 
-
+		[HttpGet]
 		public ActionResult ApplicantProfilePage()
 		{
 			var loggedInUserId = (Membership.GetUser(User.Identity.Name) as CustomMembershipUser).UserId;
@@ -292,7 +301,7 @@ namespace Recruiter.Controllers
 												 }).ToList(),
 								 // Language = dbContext.Languages.Select(x => x.ApplicantId == p.Id, new Language { Name=})
 								 //Skills = (from details in dbContext.Applicants
-									//	   where details.A )
+								 //	   where details.A )
 
 							 }).FirstOrDefault();
 
@@ -425,3 +434,11 @@ namespace Recruiter.Controllers
 		}
 	}
 }
+
+	//[HttpGet]
+	//	public ActionResult UploadImage()
+	//	{
+	//		return View();
+	//	}
+	//}
+
